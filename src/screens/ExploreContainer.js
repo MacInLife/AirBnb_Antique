8f8ce595026e7c348332019e049ea4171ca305d5
Audie MALLOGGIA [APP2B] <audie@app2b.io>
2020-04-17 16:56:04 +0200

    feature(exo): loading spinner on experiences

--- src/screens/ExploreContainer.js
+++ src/screens/ExploreContainer.js
@@ -1,24 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View,
-  Image,
   ScrollView,
   TouchableOpacity,
   ActivityIndicator,
 } from 'react-native';
 import styles from './styles';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Categories from '../components/categories'; //Intégration du composants Catégories
 import Experiences from '../components/experiences';
 import Homes from '../components/homes';
 import Popular from '../components/popular';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {Actions} from '../actions';
+import { Actions } from '../actions';
 
 class ExploreContainer extends Component {
-  static navigationOptions = ({navigation}) => ({
-    header: props => (
+  static navigationOptions = ({ navigation }) => ({
+    header: (props) => (
       <View style={styles.containerConnect}>
         <Icon size={20} style={styles.iconclose} name="close"></Icon>
         <TouchableOpacity onPress={() => navigation.navigate('Login')}>
@@ -27,49 +26,63 @@ class ExploreContainer extends Component {
       </View>
     ),
   });
-  componentDidMount() {
-    const {setListings} = this.props;
-    
-    //Récupération des données contenus dans l'URL
-    return (
-      fetch('https://api.myjson.com/bins/61fqq') // requête vers l'API
-        .then(response => response.json())
-        //Remplacement par utilisation de REDUX
-        .then(results => {
-          setListings(results);
-        })
-    );
 
-    return results;
+  // Initial state
+  state = {
+    isLoading: false
   }
-  render() {
-    const {categories, experiences, homes, popular} = this.props;
 
+  componentDidMount() {
+    return this.fetchData()
+  }
+  fetchData = () => {
+    this.setState({
+      isLoading: true
+    })
+    return fetch('https://api.myjson.com/bins/61fqq') // requête vers l'API
+      .then(response => response.json())
+      .then(results => {
+        setListings(results);
+        this.setState({
+          isLoading: false
+        })
+      })
+      .catch(() => {
+        this.setState({
+          isLoading: false
+        })
+      })
+
+  }
+
+  render() {
+    const { categories, experiences, homes, popular } = this.props;
+    const { isLoading } = this.state
     return (
-      <ScrollView>
-        <View style={[styles.container, styles.horizontal]}>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </View>
-        <View>
-          <Text style={styles.titres}>Explore Airbnb</Text>
-          <Categories categories={categories} />
-        </View>
-        <View>
-          <Text style={styles.titresExp}>Experiences</Text>
-          <Text style={[styles.textVoirPlus]}>Voir tous ></Text>
-          <Experiences experiences={experiences} />
-        </View>
-        <View>
-          <Text style={styles.titresExp}>Homes</Text>
-          <Text style={[styles.textVoirPlus]}>Voir tous ></Text>
-          <Homes homes={homes} />
-        </View>
-        <View>
-          <Text style={styles.titresExp}>Popular Reservations</Text>
-          <Text style={[styles.textVoirPlus]}>Voir tous ></Text>
-          <Popular popular={popular} />
-        </View>
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        {isLoading && <ActivityIndicator size="large" color="#red" animating={isLoading} />}
+        <ScrollView>
+          <View>
+            <Text style={styles.titres}>Explore Airbnb</Text>
+            <Categories categories={categories} />
+          </View>
+          <View>
+            <Text style={styles.titresExp}>Experiences</Text>
+            <Text style={[styles.textVoirPlus]}>Voir tous ></Text>
+            <Experiences experiences={experiences} />
+          </View>
+          <View>
+            <Text style={styles.titresExp}>Homes</Text>
+            <Text style={[styles.textVoirPlus]}>Voir tous ></Text>
+            <Homes homes={homes} />
+          </View>
+          <View>
+            <Text style={styles.titresExp}>Popular Reservations</Text>
+            <Text style={[styles.textVoirPlus]}>Voir tous ></Text>
+            <Popular popular={popular} />
+          </View>
+        </ScrollView>
+      </View>
     );
   }
 }
