98c6c4b6031fd4248b0adecda76da8e16f160c50
MacInLife <marie.ange.coco@gmail.com>
2020-05-15 09:42:09 +0200

    Intégration Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import { createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 import reducers from '../reducers';
 
@@ -8,12 +8,32 @@ import reducers from '../reducers';
 import AsyncStorage from '@react-native-community/async-storage';
 
 const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage,
-}
-const persistedReducer = persistReducer(persistConfig, reducers)
+  key: 'root',
+  storage: AsyncStorage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+const simpleLogger = store => next => action => {
+  console.log(action);
+  next(action);
+};
+const extendedLogger = store => next => action => {
+  console.log('previous state', store.getState());
+  const result = next(action);
+  console.log('next state', store.getState());
+  return;
+};
+const incrementLogger = store => next => action => {
+  if (action.type === 'INCREMENT') {
+    console.log(action);
+  }
+  next(action);
+};
 
 // composeWithDevTools() permet le debuggage dans React Native Debugger
-// 
-export const store = createStore(persistedReducer, __DEV__ ? composeWithDevTools() : undefined);
-export const persistor = persistStore(store)
+//
+export const store = createStore(
+  persistedReducer,
+  __DEV__ ? composeWithDevTools() : undefined,
+  applyMiddleware(simpleLogger, extendedLogger, incrementLogger),
+);
+export const persistor = persistStore(store);
